89de46e1d4efb064f6647c40ac3040daded1eea9
BitNinja01 <bitninja01@pm.me>
2026-05-28 13:14:01 -0700

    release: v1.4.0

--- plugin.py
+++ plugin.py
@@ -17,7 +17,7 @@ class CartographerPlugin(PinSheetPlugin):
     """Adds course geometry, hole diagrams, and yardage book generation to PinSheet."""
 
     name = "cartographer"
-    version = "1.3.0"
+    version = "1.4.0"
 
     def __init__(self) -> None:
         super().__init__()
